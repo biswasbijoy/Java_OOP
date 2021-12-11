4c57c925e519d8d1fec0c0902c9732b514b3d432
biswasbijoy <biswasbijoy414@gmail.com>
2021-12-12 00:39:29 +0600

    Label Frame practice

---
+++ HW9/src/LabelTest.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class LabelTest {
+        
+        public static void main(String[] args) {
+                
+                LabelFrame labelFrame = new LabelFrame();
+
+                // JFrame app = new JFrame();
+                
+
+                labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                labelFrame.setSize(260, 180);
+                labelFrame.setVisible(true);
+                
+
+        }
+
+}
