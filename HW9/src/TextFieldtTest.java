3b3b4b8fcf1aee7e741b74c7149485f5dd580714
biswasbijoy <biswasbijoy414@gmail.com>
2021-12-12 09:46:49 +0600

    textfield and password field

---
+++ HW9/src/TextFieldtTest.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+public class TextFieldtTest {
+        
+        public static void main(String[] args) {
+                
+                TextFieldFrame textFieldFrame = new TextFieldFrame();
+                textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                textFieldFrame.setSize(350, 100);
+                textFieldFrame.setVisible(true);
+
+        }
+
+}
